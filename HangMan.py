668116fdde39db1fc9c96db8c9d34f213e8b2af8
MOH MOH SAN <violalwin@gmail.com>
2019-03-12 18:21:16 +0800

    Adding the image in folder with HangMan Logic File

---
+++ HangMan.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 12 17:45:02 2019
+
+@author: MOH MOH SAN
+"""
+
+class HangMan:
+    HangManPics = ["1.png","2.png","3.png","4.png","5.png","6.png"]
+    
+    def getRandomWord():
+        
+        
